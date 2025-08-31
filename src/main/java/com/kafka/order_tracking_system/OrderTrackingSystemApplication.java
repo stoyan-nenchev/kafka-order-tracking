d2e24be2b42c5008102679792fc6a1355466f6cf
stoyannenchev <kaaremascode@gmail.com>
2025-08-31 16:25:39 +0300

    Adding initial project

---
+++ src/main/java/com/kafka/order_tracking_system/OrderTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.order_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/order_tracking_system/OrderTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.order_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
